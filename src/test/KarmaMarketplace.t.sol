f463139412498f24694edf2ba06e528fc216bf5d
mxnares <69873782+mxnares@users.noreply.github.com>
2022-07-12 23:13:54 -0700

    Added tests. Altered IBounty attributes

--- src/test/KarmaMarketplace.t.sol
+++ src/test/KarmaMarketplace.t.sol
@@ -1,13 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "ds-test/test.sol";
+import "ds-test/Test.sol";
 import "forge-std/console2.sol";
 import "forge-std/Vm.sol";
 import "forge-std/console.sol";
 
 import { IOrg } from "../interface/IOrg.sol";
-import { KarmaToken} from "../KarmaToken.sol";
+import { IBounty } from "../interface/IBounty.sol";
+import { KarmaToken } from "../KarmaToken.sol";
+import { CustomBounty } from "../specific-tasks/CustomBounty.sol";
 import { Treasury } from "../Treasury.sol";
 import { KarmaMarketplace } from "../KarmaMarketplace.sol";
 
@@ -41,4 +43,38 @@ contract ContractTest is DSTest {
         assertEq(_celo.name(), "Celo");
         assertEq(_celo.owner(), deployer);
     }
+
+    function testListBounty() public {
+        marketplace.createOrg("Donut");
+        IOrg _donut = marketplace.orgs(1);
+        marketplace.listBounty(1, "Eat Donut", 1, 2, 3);
+        IBounty _sample = marketplace.bounties(1);
+        assertEq(marketplace.bountyIdToOrg(1), 1);
+        assertEq(_sample.title(), "Eat Donut");
+       
+    }
+
+    function testCheckDeadline() public {
+        marketplace.createOrg("Donut");
+        IOrg _donut = marketplace.orgs(1);
+        marketplace.listBounty(1, "Eat Donut", 1, 1000000000, 0);
+        IBounty _sample = marketplace.bounties(1);
+        vm.warp(999999999);
+        assertTrue(_sample.checkDeadline());
+        vm.warp(1000000001);
+        assertTrue(!_sample.checkDeadline());
+    }
+
+    function testOpenBounty() public {
+        marketplace.createOrg("Donut");
+        IOrg _donut = marketplace.orgs(1);
+        marketplace.listBounty(1, "Eat Donut", 1, 1000000000, 0);
+        IBounty _sample = marketplace.bounties(1);
+        assertTrue(!_sample.isOpen());
+        _sample.open();
+        assertTrue(_sample.isOpen());
+
+        // marketplace.openBounty(1) does not work because caller is not the owner ( == deployer ). How to simulate this?
+    }
+    
 }
